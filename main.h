e354b3f4c633e76fbf1d258d28d81150c6015c11
omuabormalin <omuabormalin@gmail.com>
2022-10-18 06:50:31 -0700

    a new head file

--- main.h
+++ main.h
@@ -1,120 +1,151 @@
-#ifndef _PRINTF_H_
-#define _PRINTF_H_
+#ifndef MAIN_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
+#define MAIN_H
+
+
+
 #include <stdlib.h>
 
-#define OUTPUT_BUF_SIZE 1024
-#define BUF_FLUSH -1
+#include <stdarg.h>
 
-#define FIELD_BUF_SIZE 50
 
-#define NULL_STRING "(null)"
-
-#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
 
 /**
- * struct parameters - parameters struct
  *
- * @unsign: flag if unsigned value
+ *  * struct flags - struct containing flags to "turn on"
  *
- * @plus_flag: on if plus_flag specified
- * @space_flag: on if hashtag_flag specified
- * @hashtag_flag: on if _flag specified
- * @zero_flag: on if _flag specified
- * @minus_flag: on if _flag specified
+ *   * when a flag specifier is passed to _printf()
  *
- * @width: field width specified
- * @precision: field precision specified
+ *    * @plus: flag for the '+' character
  *
- * @h_modifier: on if h_modifier is specified
- * @l_modifier: on if l_modifier is specifies
+ *     * @space: flag for the ' ' character
  *
- */
-typedef struct parameters
+ *      * @hash: flag for the '#' character
+ *
+ *       */
+
+typedef struct flags
+
 {
-	unsigned int unsign			: 1;
 
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
+		int plus;
 
-	unsigned int width;
-	unsigned int precision;
+			int space;
 
-	unsigned int h_modifier		: 1;
-	unsigned int l_modifier		: 1;
-} params_t;
+				int hash;
+
+} flags_t;
+
+
+
 /**
- * struct specifier - Struct token
  *
- * @specifier: format token
- * @f: The function associated
- */
-typedef struct specifier
+ *  * struct printHandler - struct to choose the right function depending
+ *
+ *   * on the format specifier passed to _printf()
+ *
+ *    * @c: format specifier
+ *
+ *     * @f: pointer to the correct printing function
+ *
+ *      */
+
+typedef struct printHandler
+
 {
-	char *specifier;
-	int (*f)(va_list, params_t *);
-} specifier_t;
 
-/* _put.c module */
-int _puts(char *str);
-int _putchar(int c);
+		char c;
 
-/* print_functions.c module */
-int print_char(va_list ap, params_t *params);
-int print_int(va_list ap, params_t *params);
-int print_string(va_list ap, params_t *params);
-int print_percent(va_list ap, params_t *params);
-int print_S(va_list ap, params_t *params);
+			int (*f)(va_list ap, flags_t *f);
 
-/* number.c module */
-char *convert(long int num, int base, int flags, params_t *params);
-int print_unsigned(va_list ap, params_t *params);
-int print_address(va_list ap, params_t *params);
+} ph;
 
-/* specifier.c module */
-int (*get_specifier(char *s))(va_list ap, params_t *params);
-int get_print_func(char *s, va_list ap, params_t *params);
-int get_flag(char *s, params_t *params);
-int get_modifier(char *s, params_t *params);
-char *get_width(char *s, params_t *params, va_list ap);
 
-/* convert_number.c module */
-int print_hex(va_list ap, params_t *params);
-int print_HEX(va_list ap, params_t *params);
-int print_binary(va_list ap, params_t *params);
-int print_octal(va_list ap, params_t *params);
 
-/* simple_printers.c module */
-int print_from_to(char *start, char *stop, char *except);
-int print_rev(va_list ap, params_t *params);
-int print_rot13(va_list ap, params_t *params);
+/* print_nums */
 
-/* print_number.c module */
+int print_int(va_list l, flags_t *f);
 
-int _isdigit(int c);
-int _strlen(char *s);
-int print_number(char *str, params_t *params);
-int print_number_right_shift(char *str, params_t *params);
-int print_number_left_shift(char *str, params_t *params);
+void print_number(int n);
 
-/* params.c module */
-void init_params(params_t *params, va_list ap);
+int print_unsigned(va_list l, flags_t *f);
 
-/* string_fields.c modoule */
-char *get_precision(char *p, params_t *params, va_list ap);
+int count_digit(int i);
 
-/* _prinf.c module */
+
+
+/* print_bases */
+
+int print_hex(va_list l, flags_t *f);
+
+int print_hex_big(va_list l, flags_t *f);
+
+int print_binary(va_list l, flags_t *f);
+
+int print_octal(va_list l, flags_t *f);
+
+
+
+/* converter */
+
+char *convert(unsigned long int num, int base, int lowercase);
+
+
+
+/* _printf */
+
 int _printf(const char *format, ...);
 
+
+
+/* get_print */
+
+int (*get_print(char s))(va_list, flags_t *);
+
+
+
+/* get_flag */
+
+int get_flag(char s, flags_t *f);
+
+
+
+/* print_alpha */
+
+int print_string(va_list l, flags_t *f);
+
+int print_char(va_list l, flags_t *f);
+
+
+
+/* write_funcs */
+
+int _putchar(char c);
+
+int _puts(char *str);
+
+
+
+/* print_custom */
+
+int print_rot13(va_list l, flags_t *f);
+
+int print_rev(va_list l, flags_t *f);
+
+int print_bigS(va_list l, flags_t *f);
+
+
+
+/* print_address */
+
+int print_address(va_list l, flags_t *f);
+
+
+
+/* print_percent */
+
+int print_percent(va_list l, flags_t *f);
+
+
+
 #endif
-/*the end of prototypes of functions used*/
